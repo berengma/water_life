a21217ce737d7bc3695d1a5a45494367a21144b1
berengma <Gundul@mailbox.org>
2019-10-05 18:50:50 +0200

    add sonar to wahles

--- init.lua
+++ init.lua
@@ -46,9 +46,38 @@ local function whale_brain(self)
 		mobkit.hq_die(self)
 		return
 	end
-	
+    
+    
+    -- big animals need to avoid obstacles
+    
+    
+    if mobkit.timer(self,10) then
+        local yaw =  self.object:get_yaw() + pi
+        local cleft = math.floor((yaw - 0.01)*100)/100
+        local cright = math.floor((yaw + 0.01)*100)/100
+        
+        local pos = mobkit.get_stand_pos(self)
+        
+        local cpos = mobkit.pos_translate2d(pos,cleft,20)
+        local c2pos = mobkit.pos_translate2d(pos,cright,20)
+        c2pos = mobkit.pos_shift(c2pos,{y=-4})
+        yaw = yaw - pi
+        
+        local checker= minetest.find_nodes_in_area(cpos,c2pos, {"group:water"})
+        --minetest.chat_send_all(dump(#checker))
+        if #checker < 5 then
+            mobkit.clear_queue_high(self)
+            mobkit.hq_aqua_turn(self,30,yaw+(pi/4),1)
+        end
+    end
+        
+    
 	if mobkit.is_queue_empty_high(self) then mobkit.hq_aqua_roam(self,20,-1) end
 end
+
+
+
+
 -- spawning is too specific to be included in the api, this is an example.
 -- a modder will want to refer to specific names according to games/mods they're using 
 -- in order for mobs not to spawn on treetops, certain biomes etc.
@@ -179,71 +208,3 @@ minetest.register_entity("water_life:whale",{
 })
 
 minetest.register_globalstep(spawnstep)
-
-
---[[
-
-mobs:register_mob("dmobs:whale", {
-	type = "animal",
-	passive = false,
-	reach = 1,
-	damage = 2,
-	attack_type = "dogfight",
-	hp_min = 52,
-	hp_max = 82,
-	armor = 230,
-	collisionbox = {-0.9, -1.2, -0.9, 0.9, 0.9, 0.9},
-	visual = "mesh",
-	mesh = "whale.b3d",
-	textures = {
-		{"dmobs_whale.png"},
-	},
-	blood_texture = "mobs_blood.png",
-	visual_size = {x=2.5, y=2.5},
-	makes_footstep_sound = true,
-	walk_velocity = 0.5,
-	run_velocity = 1,
-	jump = false,	
-	stepheight = 1.5,
-	fall_damage = 0,
-	fall_speed = -6,
-	fly = true,
-	fly_in = "default:water_source",
-	water_damage = 0,
-	lava_damage = 2,
-	light_damage = 0,
-	follow = {"fishing:fish_cooked"},
-	view_range = 14,
-	drops = {
-		{name = "mobs:meat_raw", chance = 1, min = 10, max = 25},
-	},
-	sounds = {
-      random = "whale_1",
-      death = "whale_1",
-      distance = 128,
-	},
-	animation = {
-		speed_normal = 5,
-		speed_run = 10,
-		walk_start = 2,
-		walk_end = 39,
-		stand_start = 2,
-		stand_end = 39,
-		run_start = 2,
-		run_end = 39,
-
-	},
-	on_rightclick = function(self, clicker)
-
-		if mobs:feed_tame(self, clicker, 8, true, true) then
-			return
-		end
-
-		mobs:capture_mob(self, clicker, 0, 5, 50, false, nil)
-	end,
-})
-
-mobs:register_spawn("dmobs:whale", {"default:water_source"}, 20, 0, 25000, 5, -2)
-
-mobs:register_egg("dmobs:whale", "Whale", "default_water_source.png", 1)
-]]
