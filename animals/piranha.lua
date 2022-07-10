50676fbe48b6519e300fe2a566df8de3dc63dbec
Gundul <sampah@mailbox.org>
2022-07-10 14:18:24 +0200

    clean piranha

--- animals/piranha.lua
+++ animals/piranha.lua
@@ -6,19 +6,18 @@ water_life.piranha_biomes= {	["rainforest"]=1,
 						["savanna_ocean"]=1,
 }
 
-
 local function piranha_ff(self)
 	for i = 1,#water_life.shark_food,1 do
-		if water_life.shark_food[i] ~= "water_life:fish" and water_life.shark_food[i] ~= "water_life:fish_tamed" then
-			local target = mobkit.get_closest_entity(self,water_life.shark_food[i])
-			if target then
-				return target
-			end
+		if water_life.shark_food[i] ~= "water_life:fish" and water_life.shark_food[i] ~=
+			"water_life:fish_tamed" then
+				local target = mobkit.get_closest_entity(self,water_life.shark_food[i])
+				if target then
+					return target
+				end
 		end
 	end
 	return nil
 end
-
 
 local function piranha_brain(self)
 	if not mobkit.is_alive(self) then	
@@ -29,19 +28,19 @@ local function piranha_brain(self)
 	end
 	if mobkit.timer(self,1) then 
         if not self.isinliquid	then 
-            --minetest.chat_send_all(dump(self.isinliquid))
             mobkit.hurt(self,1)
         end
         local target = mobkit.get_nearby_player(self)
-		
         if target then
 			local dist = water_life.dist2tgt(self,target)
 			mobkit.animate(self,"fast")
-			if dist > 2 and mobkit.is_alive(target) and mobkit.is_in_deep(target) and target:get_attach() == nil then
-				mobkit.clear_queue_high(self)
-				water_life.hq_water_attack(self,target,50,5)
-			elseif target and mobkit.is_alive(target) and ((not mobkit.is_in_deep(target)) or (target:get_attach() ~= nil)) then
-				water_life.hq_swimfrom(self,30,target,4,5)
+			if dist > 2 and mobkit.is_alive(target) and mobkit.is_in_deep(target)
+				and target:get_attach() == nil then
+					mobkit.clear_queue_high(self)
+					water_life.hq_water_attack(self,target,50,5)
+			elseif target and mobkit.is_alive(target) and ((not mobkit.is_in_deep(target))
+				or (target:get_attach() ~= nil)) then
+					water_life.hq_swimfrom(self,30,target,4,5)
 			end
 		end
 		local prio = mobkit.get_queue_priority(self)
@@ -55,12 +54,9 @@ local function piranha_brain(self)
 				end
 			end
 		end
-		
-	
         if self.isinliquid and self.isinliquid == "default:water_source" then
-			
-			water_life.hq_swimto(self,20,1,{"water_life:muddy_river_water_source","default:river_water_source"})
-			
+			water_life.hq_swimto(self,20,1,{"water_life:muddy_river_water_source",
+				"default:river_water_source"})
         end
         if mobkit.is_queue_empty_high(self) then
             mobkit.animate(self,"def")
@@ -69,15 +65,7 @@ local function piranha_brain(self)
     end
 end
 
-
----------------
--- the Entities
----------------
-
-
-
 minetest.register_entity("water_life:piranha",{
-											-- common props
 	physical = true,
 	stepheight = 0.3,				
 	collide_with_objects = false,
@@ -85,19 +73,17 @@ minetest.register_entity("water_life:piranha",{
 	visual = "mesh",
 	mesh = "water_life_piranha.b3d",
 	textures = {"water_life_piranha.png"},
-	visual_size = {x = 0.3, y = 0.3}, --2.5
+	visual_size = {x = 0.3, y = 0.3},
 	static_save = true,
 	makes_footstep_sound = true,
-	on_step = mobkit.stepfunc,	-- required
-	on_activate = mobkit.actfunc,		-- required
+	on_step = mobkit.stepfunc,
+	on_activate = mobkit.actfunc,
 	get_staticdata = mobkit.statfunc,
-											-- api props
 	springiness=0,
-	buoyancy = 1.0,					-- portion of hitbox submerged
+	buoyancy = 1.0,
 	max_speed = 6,                     
 	jump_height = 0.5,
 	view_range = 16,
---	lung_capacity = 0, 		-- seconds
 	max_hp = 10,
 	timeout=360,
     wild = true,
@@ -107,26 +93,22 @@ minetest.register_entity("water_life:piranha",{
 		{name = "water_life:meat_raw", chance = 2, min = 1, max = 1,},
 	},
     animation = {
-		def={range={x=1,y=20},speed=10,loop=true},	--35
+		def={range={x=1,y=20},speed=10,loop=true},
 		fast={range={x=1,y=20},speed=20,loop=true},
-        --idle={range={x=36,y=75},speed=20,loop=true},
 		},
 	brainfunc = piranha_brain,
 	on_punch=function(self, puncher, time_from_last_punch, tool_capabilities, dir)
 		if mobkit.is_alive(self) then
 			if water_life.bloody then water_life.spilltheblood(self.object) end			
 			mobkit.hurt(self,tool_capabilities.damage_groups.fleshy or 1)
-
 		end
 	end,
     on_rightclick = function(self, clicker)
         if not clicker or not clicker:is_player() then return end
         local inv = clicker:get_inventory()
         local item = clicker:get_wielded_item()
-        
         if not item or item:get_name() ~= "fireflies:bug_net" then return end
         if not inv:room_for_item("main", "water_life:fish") then return end
-                                            
         inv:add_item("main", "water_life:piranha")
         self.object:remove()
     end,
@@ -134,7 +116,6 @@ minetest.register_entity("water_life:piranha",{
 
 
 minetest.register_entity("water_life:piranha_tamed",{
-											-- common props
 	physical = true,
 	stepheight = 0.3,				
 	collide_with_objects = false,
@@ -145,16 +126,14 @@ minetest.register_entity("water_life:piranha_tamed",{
 	visual_size = {x = 0.3, y = 0.3}, --2.5
 	static_save = true,
 	makes_footstep_sound = true,
-	on_step = mobkit.stepfunc,	-- required
-	on_activate = mobkit.actfunc,		-- required
+	on_step = mobkit.stepfunc,
+	on_activate = mobkit.actfunc,
 	get_staticdata = mobkit.statfunc,
-											-- api props
 	springiness=0,
-	buoyancy = 1.0,					-- portion of hitbox submerged
+	buoyancy = 1.0,
 	max_speed = 6,                     
 	jump_height = 0.5,
 	view_range = 16,
---	lung_capacity = 0, 		-- seconds
 	max_hp = 10,
 	wild = false,
 	attack={range=0.4,damage_groups={fleshy=5}},
@@ -163,26 +142,23 @@ minetest.register_entity("water_life:piranha_tamed",{
 		{name = "water_life:meat_raw", chance = 2, min = 1, max = 1,},
 	},
     animation = {
-		def={range={x=1,y=20},speed=10,loop=true},	--35
+		def={range={x=1,y=20},speed=10,loop=true},
 		fast={range={x=1,y=20},speed=20,loop=true},
-        --idle={range={x=36,y=75},speed=20,loop=true},
 		},
 	brainfunc = piranha_brain,
 	on_punch=function(self, puncher, time_from_last_punch, tool_capabilities, dir)
 		if mobkit.is_alive(self) then
 			if water_life.bloody then water_life.spilltheblood(self.object) end			
 			mobkit.hurt(self,tool_capabilities.damage_groups.fleshy or 1)
-
 		end
 	end,
 	on_rightclick = function(self, clicker)
         if not clicker or not clicker:is_player() then return end
         local inv = clicker:get_inventory()
         local item = clicker:get_wielded_item()
-        
-        if not item or (item:get_name() ~= "fireflies:bug_net" and item:get_name() ~= water_life.catchNet) then return end
+        if not item or (item:get_name() ~= "fireflies:bug_net" and 
+			item:get_name() ~= water_life.catchNet) then return end
         if not inv:room_for_item("main", "water_life:fish") then return end
-                                            
         inv:add_item("main", "water_life:piranha")
         self.object:remove()
     end,
