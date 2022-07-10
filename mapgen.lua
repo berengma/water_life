40e2889dd05ddf72382f082773af9232f5b2f871
Gundul <sampah@mailbox.org>
2022-07-10 12:56:30 +0200

    cleanup mapgen

--- mapgen.lua
+++ mapgen.lua
@@ -1,6 +1,4 @@
-
 if minetest.registered_biomes["rainforest"] then
-	
 	local rainforest = minetest.registered_biomes["rainforest"]
 	rainforest.node_river_water = "water_life:muddy_river_water_source"
 	rainforest.node_riverbed = "default:dirt"
@@ -11,10 +9,7 @@ if minetest.registered_biomes["rainforest"] then
 	minetest.register_biome(rainforest)
 end
 
-
-
 if minetest.registered_biomes["rainforest_ocean"] then
-	
 	local rainforest = minetest.registered_biomes["rainforest_ocean"]
 	rainforest.node_river_water = "water_life:muddy_river_water_source"
 	rainforest.node_riverbed = "default:dirt"
@@ -25,10 +20,7 @@ if minetest.registered_biomes["rainforest_ocean"] then
 	minetest.register_biome(rainforest)
 end
 
-
-
 if minetest.registered_biomes["rainforest_swamp"] then
-	
 	local rainforest = minetest.registered_biomes["rainforest_swamp"]
 	rainforest.node_river_water = "water_life:muddy_river_water_source"
 	rainforest.node_riverbed = "default:dirt"
@@ -39,18 +31,11 @@ if minetest.registered_biomes["rainforest_swamp"] then
 	minetest.register_biome(rainforest)
 end
 
-
-
 if minetest.registered_biomes["savanna"] then
-	
 	local savanna = minetest.registered_biomes["savanna"]
 	savanna.node_river_water = "water_life:muddy_river_water_source"
 	savanna.node_riverbed = "default:sand"
 	savanna.depth_riverbed = 2
---	savanna.node_water_top = "water_life:muddy_river_water_source"
---	savanna.depth_water_top = 10
 	minetest.unregister_biome("savanna")
 	minetest.register_biome(savanna)
 end
-
-
