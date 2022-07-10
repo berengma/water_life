fbc6242e6a8e33d0ccb01540e290f5d2eb116afe
Gundul <sampah@mailbox.org>
2022-07-10 14:10:07 +0200

    clean gulls

--- animals/gulls.lua
+++ animals/gulls.lua
@@ -3,8 +3,6 @@ local rad = math.rad
 
 water_life.register_shark_food("water_life:gull")
 
-
-
 local function gull_brain(self)
 	if not mobkit.is_alive(self) then	
 		mobkit.clear_queue_high(self)
@@ -14,131 +12,84 @@ local function gull_brain(self)
 		return
 	end
 	
-	--
-	--get prorities !
 	local prty = mobkit.get_queue_priority(self)
 	-- 
 	-- 10-19 fly		20-29 water		30-39 land
 	--
-	
-	
-	
 	if mobkit.timer(self,10) then
 		local rnd = random (100)
 		local force = false
-		
 		if rnd < 11 then
 			mobkit.make_sound(self,"idle")
 		end
-		
 		local plyr = mobkit.get_nearby_player(self)
 		local whale = mobkit.get_nearby_entity(self,"water_life:whale")
-		
-		
-		
 		local wname = ""
-		
 		if whale and rnd > 10 then
-			--minetest.chat_send_all("WHALES !")
 			mobkit.clear_queue_high(self)
 			mobkit.clear_queue_low(self)
 			water_life.hq_fly2obj(self,18,whale,4,true)
 		end
-			
-		
 		if plyr then
 			local stack = plyr:get_wielded_item()
 			wname = stack:get_name()
 			if rnd < 10 then force = true end
-			--minetest.chat_send_all("YOU HOLD bait "..dump(water_life.gull_bait[wname]))
 		end
-		
 		if plyr and prty < 17 and (water_life.gull_bait[wname] or force) then
-			--minetest.chat_send_all("MATCH")
 			mobkit.clear_queue_high(self)
 			mobkit.clear_queue_low(self)
 			water_life.hq_fly2obj(self,18,plyr,2,force)
 		end
-			
 	end
-	
-	-- die if crashed somewhere
 	if mobkit.timer(self,5) then
 		local vel = self.object:get_velocity()
 		local speed = vector.length(vel)
 		if speed < 1 and not self.isinliquid then
 			mobkit.clear_queue_high(self)
-			--mobkit.hq_die(self)
 			water_life.hq_die(self,'float')
 		end
 	end
-	
-	
-	--land, water and air
 	if mobkit.timer(self,5) then
-		
 		local land = mobkit.recall(self,"landlife")
 		local water = mobkit.recall(self,"waterlife")
 		local air = mobkit.recall(self,"airlife")
-		
 		if air then
 			air = math.floor(os.time()-air)
 			if random(500) < air then
-				--minetest.chat_send_all("Time to land")
 				mobkit.clear_queue_high(self)
 				mobkit.clear_queue_low(self)
-				--water_life.hq_landon_water(self,15)
 			end
 		end
 		
 		if land then
 			land = math.floor(os.time()-land)
 			if random(500) < land then
-				--minetest.chat_send_all("Land takeoff")
 				mobkit.clear_queue_high(self)
 				mobkit.clear_queue_low(self)
-				--water_life.hq_takeoff(self,15)
-				
 			end
 		end
-		
 		if water then
 			water = math.floor(os.time()-water)
 			if random (500) < water then
-				--minetest.chat_send_all("Go to land")
 				mobkit.clear_queue_high(self)
 				mobkit.clear_queue_low(self)
 				water_life.hq_go2land(self,15)
 			end
 		end
-		
-		--minetest.chat_send_all("Land: "..dump(land).." :  Water: "..dump(water))
 	end
-	
-	
-	
 	if mobkit.timer(self,1) then
 		local enemy = water_life.get_closest_enemy(self)
-		--minetest.chat_send_all(dump(prty)..")  "..dump(self.isinliquid)) --.."    :    "..dump(self.dtime)) --colinfo.collides))
-		
 		if self.isinliquid then
-			
 			mobkit.remember(self,"waterlife",os.time())
 			mobkit.forget(self,"landlife")
 			mobkit.forget(self,"airlife")
-			
-			
-			
 			if prty > 19 and prty < 22 and enemy then
-				--minetest.chat_send_all("WARNING: "..dump(enemy:get_luaentity().name))
 				local eyaw = enemy:get_yaw()
 				mobkit.clear_queue_high(self)
 				mobkit.clear_queue_low(self)
 				water_life.hq_water_takeoff(self,22,'takeoff',eyaw)
 				water_life.hq_climb(self,15,4,16)
 			end
-			
-			-- anything else from inwater behavior
 			if prty < 20 or prty > 30 then 
 				mobkit.clear_queue_high(self)
 				mobkit.clear_queue_low(self)
@@ -146,21 +97,14 @@ local function gull_brain(self)
 				water_life.hq_water_takeoff(self,20,'takeoff')
 				water_life.hq_climb(self,15,4,16)
 			end
-			
 		end
-		
 		if self.isonground then
-			
 			mobkit.remember(self,"landlife",os.time())
 			mobkit.forget(self,"waterlife")
 			mobkit.forget(self,"airlife")
-			
 		end
 	end
-	
-	
 	if mobkit.is_queue_empty_high(self) then
-		
 		if not self.isinliquid then
 			self.object:add_velocity({x=2,y=4,z=2})
 			water_life.hq_climb(self,15,4,16)
@@ -172,21 +116,10 @@ local function gull_brain(self)
 			water_life.hq_water_takeoff(self,20,'takeoff')
 			water_life.hq_climb(self,15,4,16)
 		end
-		
 	end
-	
-	
 end
 
-
----------------
--- the Entities
----------------
-
-
-
 minetest.register_entity("water_life:gull",{
-											-- common props
 	physical = true,
 	stepheight = 0.5,				
 	collide_with_objects = false,
@@ -197,22 +130,18 @@ minetest.register_entity("water_life:gull",{
 	visual_size = {x = 0.75, y = 0.5, z = 0.75},
 	static_save = true,
 	makes_footstep_sound = false,
-	on_step = mobkit.stepfunc,	-- required
-	on_activate = mobkit.actfunc,		-- required
+	on_step = mobkit.stepfunc,
+	on_activate = mobkit.actfunc,
 	get_staticdata = mobkit.statfunc,
-											-- api props
 	springiness=0,
-	buoyancy = 0.59,					-- portion of hitbox submerged
+	buoyancy = 0.59,
 	max_speed = 3,                     
 	jump_height = 1.5,
-	view_range = water_life.abo * 16,		-- max what server can handle, birds need good eyes !
---	lung_capacity = 0, 		-- seconds
+	view_range = water_life.abo * 16,
 	max_hp = 5,
 	timeout=10,
 	wild = true,
 	drops = {},
-	--	{name = "default:diamond", chance = 20, min = 1, max = 1,},		
-	--	{name = "water_life:meat_raw", chance = 2, min = 1, max = 1,},
 	predators = {["water_life:shark"] = 1,
                   ["water_life:alligator"] = 1,
                   ["water_life:whale"] = 1,
@@ -261,4 +190,3 @@ minetest.register_entity("water_life:gull",{
 		end
 	end,
 })
-
